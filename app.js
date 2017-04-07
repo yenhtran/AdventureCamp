6d115b92b8cc48df766d0d077c4161b5ce48bf78
Yen Tran <tran.yentran@gmail.com>
2017-04-07 16:31:48 +0000

    Adds views, /adventure routes

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express'),
     app = express();
     
+app.set('view engine', 'ejs');
+    
 app.get('/', function(req, res){
-    res.send('this will be the landing page soon!');
+    res.render('landing');
+});
+
+app.get('/adventures', function(req, res){
+    var adventures = [
+            {name: 'Surfing', image: 'https://source.unsplash.com/L5aI2jU0i50/400X300'},
+            {name: 'Snowboarding', image: 'https://source.unsplash.com/pOwhy6PDorE/400X300'},
+            {name: 'Rock Climbing', image: 'https://source.unsplash.com/uJfwRhfgSnw/400X300'},
+            {name: 'Hiking', image: 'https://source.unsplash.com/rHv6C-WTOls/400X300'}
+        ]
+        
+        res.render('adventures', {adventures: adventures});
 });
     
 app.listen(process.env.PORT, process.env.IP, function(){
