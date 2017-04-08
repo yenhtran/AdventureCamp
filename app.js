32305fc18785603054eb51021b30dd16754e0d8b
Yen Tran <tran.yentran@gmail.com>
2017-04-08 00:21:07 +0000

    refactors and redirect post route

--- app.js
+++ app.js
@@ -4,24 +4,29 @@ var express = require('express'),
     
 app.use(bodyParser.urlencoded({extended: true}));    
 app.set('view engine', 'ejs');
+
+var adventures = [
+            {name: 'Surfing', image: 'https://source.unsplash.com/L5aI2jU0i50/400X300'},
+            {name: 'Snowboarding', image: 'https://source.unsplash.com/pOwhy6PDorE/400X300'},
+            {name: 'Rock Climbing', image: 'https://source.unsplash.com/uJfwRhfgSnw/400X300'},
+            {name: 'Hiking', image: 'https://source.unsplash.com/rHv6C-WTOls/400X300'}
+        ]
     
 app.get('/', function(req, res){
     res.render('landing');
 });
 
 app.get('/adventures', function(req, res){
-    var adventures = [
-            {name: 'Surfing', image: 'https://source.unsplash.com/L5aI2jU0i50/400X300'},
-            {name: 'Snowboarding', image: 'https://source.unsplash.com/pOwhy6PDorE/400X300'},
-            {name: 'Rock Climbing', image: 'https://source.unsplash.com/uJfwRhfgSnw/400X300'},
-            {name: 'Hiking', image: 'https://source.unsplash.com/rHv6C-WTOls/400X300'}
-        ]
-        
         res.render('adventures', {adventures: adventures});
 });
 
 app.post('/adventures', function(req, res){
-    res.send('You hit POST ROUTE');
+    var name = req.body.name,
+        image = req.body.image,
+        newAdventure = {name: name, image: image};
+        
+    adventures.push(newAdventure);
+    res.redirect('/adventures');
 });
 
 app.get('/adventures/new', function(req, res) {
