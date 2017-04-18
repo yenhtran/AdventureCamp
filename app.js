5264313f04f7fc62dde3fc9a973127bdf333f19f
Yen Tran <tran.yentran@gmail.com>
2017-04-18 16:56:17 +0000

    adds basic register

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 seedDB();
 
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+    secret: 'The mountains are calling!',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//================
+// ROUTES
+//================
+
 app.get('/', function(req, res){
     res.render('landing');
 });
@@ -100,7 +116,30 @@ app.post('/adventures/:id/comments', function(req, res){
         }
     })
     
-})
+});
+
+//================
+// AUTH ROUTES
+//================
+
+//shows register form
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+//handle sign up logic
+app.post('/register', function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/adventures');
+        });
+    });
+});
     
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('LivinAdventures Has Started!');
