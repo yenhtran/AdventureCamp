4bb14497cf69baa74d044aa25ec9c51988b73ab9
Yen Tran <tran.yentran@gmail.com>
2017-04-18 16:18:22 +0000

    Adds User model

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
     Adventure = require('./models/adventure'),
     Comment = require('./models/comment'),
+    User = require('./models/user'),
     seedDB = require('./seeds');
     
     
